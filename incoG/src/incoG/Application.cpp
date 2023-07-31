8045dd1c1284a9a2918c7c168f195c48e14f3cf4
Chase Cotton <106311783+ChaseCotton@users.noreply.github.com>
2023-07-31 18:14:00 -0500

    Event System
    - Event Dispatching
    - Keyboard Events
    - App Events
    - Mouse Events
    
    Code outputs window resize along with inialization

--- incoG/src/incoG/Application.cpp
+++ incoG/src/incoG/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "C:\Engine\incoG\incoG\src\incoG\Events\ApplicationEvent.h"
+#include "C:\Engine\incoG\incoG\src\incoG\Log.h"
+
 namespace incoG {
 
 	Application::Application() {
@@ -11,6 +14,9 @@ namespace incoG {
 	}
 
 	void Application::Run() {
+		WindowResizeEvent e(1280, 720);
+		IG_TRACE(e);
+
 		while (true);
 	}
 
--- incoG/src/incoG/Core.h
+++ incoG/src/incoG/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error incoG only supports Windows 64x
 #endif
+
+#define BIT(x) (1 << x)
